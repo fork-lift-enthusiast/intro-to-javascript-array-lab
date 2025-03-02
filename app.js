7f5bfc3b9dda7357fb21ce09617367d8c061b1a1
bonnie <owu.owu.666@gmail.com>
2025-03-02 16:03:14 -0500

    exercises like 5 - 13 or smth

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const foods = []; 
+const foods = [];
 /*
 Exercise 2: Add strings to the array
 
@@ -9,7 +9,7 @@ Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
 Complete Exercise 2 in the space below:
 */
 
-foods.push("pizza","cheeseburger")
+foods.push("pizza", "cheeseburger");
 /*
 console.log('Exercise 2 result:', foods);
 
@@ -19,8 +19,7 @@ Exercise 3: Insert at the beginning
 
 Complete Exercise 3 in the space below:
 */
-foods.unshift("taco")
-
+foods.unshift("taco");
 
 // console.log('Exercise 3 result:', foods);
 /*
@@ -33,8 +32,7 @@ Exercise 4: Access an array element
 
 Complete Exercise 4 in the space below:
 */
-favFood = foods[1]
-
+favFood = foods[1];
 
 // console.log('Exercise 4 result:', favFood);
 /*
@@ -44,8 +42,161 @@ Exercise 5: Insert an element between two others
 
 Complete Exercise 5 in the space below:
 */
-foods.splice(2,0,'tofu')
+foods.splice(2, 0, "tofu");
 
+console.log("Exercise 5 result:", foods);
 
-console.log('Exercise 5 result:', foods);
+/*
+Exercise 6: Replace elements
 
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+foods.splice(1, 1, "sushi", "cupcake");
+
+console.log("Exercise 6 result:", foods);
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+const yummy = foods.splice(1, 2, "sushi", "cupcake");
+
+console.log("Exercise 7 result:", yummy);
+/*
+Exercise 8: Finding an index
+
+1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+   `foods` array. 
+
+2) Assign it to a variable named `soyIdx`.
+
+Complete Exercise 8 in the space below:
+*/
+const soyIdx = foods.indexOf("tofu");
+
+console.log("Exercise 8 result:", soyIdx);
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+allFoods = foods.join("->");
+
+console.log("Exercise 9 result:", allFoods);
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+hasSoup = foods.includes("soup");
+
+console.log("Exercise 10 result:", hasSoup);
+/*
+Exercise 11: Odd numbers from an array
+
+1) Choose a method to iterate through the `nums` array.
+
+2) Push each odd number to a new array named `odds`.
+
+Hint: Initialize the `odds` variable to an empty array before the iteration.
+
+Complete Exercise 11 in the space below:
+*/
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+odds = [];
+console.log(nums.length);
+for (i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 != 0) {
+    odds.push(nums[i]);
+  }
+}
+
+console.log("Exercise 11 result:", odds);
+/*
+Exercise 12: FizzBuzz with arrays
+
+1) Choose a method to iterate through the `nums` array. 
+
+2. As you loop, sort the numbers into new arrays based on the following rules:
+
+   - Push any number evenly divisible by 3 to an array called `fizz`.
+   - Push any number evenly divisible by 5 to an array called `buzz`.
+   - Push any number that is evenly divisible by 3 and 5 to an array called
+     `fizzbuzz`.
+
+   Note: A single number may meet more than one of the above rules. If it does,
+         it should be placed in multiple arrays. For example, the number `15`
+         will appear in the `fizz`, `buzz`, and `fizzbuzz` arrays.
+
+Complete Exercise 12 in the space below:
+
+*/
+
+fizz = [];
+buzz = [];
+fizzbuzz = [];
+fizzChecker = false;
+buzzCheker = false;
+console.log(nums.length);
+for (i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    fizz.push(nums[i]);
+    fizzChecker = true;
+  }
+  if (nums[i] % 5 === 0) {
+    buzz.push(nums[i]);
+    buzzCheker = true;
+  }
+  if (fizzChecker === true && buzzCheker === true) {
+    fizzbuzz.push(nums[i]);
+    fizzChecker = false;
+    buzzCheker = false;
+  }
+}
+
+console.log("Exercise 12 Results:");
+console.log("  fizz:", fizz);
+console.log("  buzz:", buzz);
+console.log("  fizzbuzz:", fizzbuzz);
+
+/*
+Exercise 13: Retrieve the Last Array
+
+1) Assign the last nested array in the `numArrays` below to a variable named
+   `numList`. As you do this, also fulfill these goals:
+
+   - Assume you don't know how many nested arrays `numArrays` contains.
+   - Do not alter the original `numArrays` array.
+
+Complete Exercise 13 in the space below:
+*/
+
+const numArrays = [
+  [100, 5, 23],
+  [15, 21, 72, 9],
+  [45, 66],
+  [7, 81, 90],
+];
+
+console.log("Exercise 13 result:", numList);
